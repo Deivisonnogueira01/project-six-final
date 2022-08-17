82d88e49068a6b4b46381d8fa1109e244adb774f
Deivison Nogueira <deivison.nogueira@live.com>
2022-08-16 20:23:46 -0400

    add typeorm

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,18 +1,20 @@
-import { CreateCatDTO } from './dto/cats.dto';
+
 import { Inject, Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
 import { CreateCat } from './entities/cat.entity';
 
 @Injectable()
 export class CatsService {
-  private cats: CreateCatDTO[] = [];
+  private cats: CreateCatDto[] = [];
 
      constructor(
         @Inject('CATS_REPOSITORY')
         private catsRepository : Repository<CreateCat>;
      ){}
 
-  create(cat: CreateCatDTO) {
+  create(cat: CreateCatDto) {
     this.cats.push(cat);
+    this.catsRepository.save();
   }
 
   findAll(): Promise<CreateCat[]> {
@@ -20,16 +22,16 @@ export class CatsService {
   }
 
   findOne(id: string) {
-    return this.cats.filter((obj: CreateCatDTO) => obj.id === id);
+    return this.cats.filter((obj: CreateCatDto) => obj.id === id);
   }
 
   remove(id: string) {
-    const cats_remove = this.cats.filter((obj: CreateCatDTO) => obj.id != id);
+    const cats_remove = this.cats.filter((obj: CreateCatDto) => obj.id != id);
     this.cats = cats_remove;
   }
 
-  update(id: string, createCatDTO: CreateCatDTO) {
-    this.cats.map((obj: CreateCatDTO) => {
+  update(id: string, createCatDTO: CreateCatDto) {
+    this.cats.map((obj: CreateCatDto ) => {
       if (obj.id === id) {
         obj.name = createCatDTO.name;
         obj.age = createCatDTO.age;
