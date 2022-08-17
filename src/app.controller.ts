22a2629f7d15546655124e9a0327159f5c36642e
Deivison Nogueira <deivison.nogueira@live.com>
2022-08-16 20:07:19 -0400

    modified app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
-  let appController: AppController;
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+  @Get('hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
-});
+  @Get('find')
+  getHello2() {
+    return 'outro hello';
+  }
+}
